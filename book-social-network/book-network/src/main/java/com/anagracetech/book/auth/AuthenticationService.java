5325cfddc2fd01054c8fae7c423b1eaf006f8bf1
Chima-Anason <tblynxx@gmail.com>
2024-04-22 01:09:48 +0800

    Implement the register function and Send Email function

---
+++ book-social-network/book-network/src/main/java/com/anagracetech/book/auth/AuthenticationService.java
@@ -0,0 +1,71 @@
+package com.anagracetech.book.auth;
+
+import com.anagracetech.book.email.EmailService;
+import com.anagracetech.book.role.RoleRepository;
+import com.anagracetech.book.user.Token;
+import com.anagracetech.book.user.TokenRepository;
+import com.anagracetech.book.user.User;
+import com.anagracetech.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final EmailService emailService;
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                //Todo: enhance exception handling
+                .orElseThrow(() -> new IllegalStateException("ROLE USER was not initialized"));
+        var user = User.builder()
+                .firstname(request.firstname())
+                .lastname(request.lastname())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+        // send email
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        // generate a token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for(int i = 0; i < length; i++){
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
+    }
+}
