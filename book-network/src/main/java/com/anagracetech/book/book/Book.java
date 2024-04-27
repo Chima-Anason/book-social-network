2eed4d9d1684f2f938e67ac60a472abcc187c818
Chima-Anason <tblynxx@gmail.com>
2024-04-27 23:31:05 +0800

    Implement find book by ID, Closes #15

--- book-network/src/main/java/com/anagracetech/book/book/Book.java
+++ book-network/src/main/java/com/anagracetech/book/book/Book.java
@@ -4,10 +4,7 @@ import com.anagracetech.book.common.BaseEntity;
 import com.anagracetech.book.feedback.Feedback;
 import com.anagracetech.book.history.BookTransactionHistory;
 import com.anagracetech.book.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,5 +38,18 @@ public class Book extends BaseEntity {
 
     @OneToMany(mappedBy = "book")
     private List<BookTransactionHistory> histories;
+
+    @Transient
+    public double getRate(){
+        if(feedbacks == null ||feedbacks.isEmpty()){
+            return 0.0;
+        }
+        var rate = this.feedbacks.stream()
+                .mapToDouble(Feedback::getNote)
+                .average()
+                .orElse(0.0);
+        double roundedRate = Math.round(rate * 10.0) / 10.0;
+        return roundedRate;
+    }
 
 }
